d8e742d0ed155ba7f74fcb6926557306df87d502
meomancer <meomancer@gmail.com>
2016-04-29 14:08:56 +0700

    search function

--- django_project/healthsites/urls.py
+++ django_project/healthsites/urls.py
@@ -17,4 +17,8 @@ urlpatterns = patterns(
     # Event related urls
     url(r'^healthsites$', HealthsitesView.as_view(), name='healthsites_view'),
     url(r'^healthsites/cluster$', 'healthsites.views.healthsites_view.get_cluster', name='healthsites_cluster'),
+    url(r'^healthsites/names', 'healthsites.views.healthsites_view.search_healthsites_name',
+        name='search_healthsites_name'),
+    url(r'^healthsites/search-name', 'healthsites.views.healthsites_view.search_healthsite_by_name',
+        name='search_healthsite_by_name'),
 )
