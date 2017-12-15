572ba2133226df03e8288e0e90208a1b6c4c5cc2
Kresimir Knezevic <kresimir.knezevic@protonmail.com>
2017-12-15 13:42:06 +0100

    Add separated working test html for drawing shapes
    Troubleshooting why shapes arent visible in project

--- django_project/event_mapper/static/event_mapper/js/event_mapper.js
+++ django_project/event_mapper/static/event_mapper/js/event_mapper.js
@@ -1,168 +1,16 @@
 // base water board module
-var WB = (function (module) {
+var WB = (function (module, leaflet) {
 
      module.mapHandler = Object.assign({}, module, {mapHandler: {}});
 //
-//     if (leaflet === false) {
-//         throw new Error('Could not load leaflet');
-//     }
-//
+    if (leaflet === false) {
+        throw new Error('Could not load leaflet');
+    }
+
 //     // TODO refactor later
-//     module.mapHandler.addEditControlToLeaflet = function () {
-//
-//          leaflet.EditControl = L.Control.extend({
-//             options: {
-//                 position: 'topright',
-//                 callback: null,
-//                 kind: '',
-//                 html: ''
-//             },
-//             onAdd: function (map) {
-//                 /*     var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
-//                 link = L.DomUtil.create('a', '', container);
-//
-//             link.href = '#';
-//             link.title = 'Create a new ' + this.options.kind;
-//             link.innerHTML = this.options.html;
-//             L.DomEvent.on(link, 'click', L.DomEvent.stop)
-//                       .on(link, 'click', function () {
-//                         window.LAYER = this.options.callback.call(map.editTools);
-//                       }, this);
-//
-//             return container;*/
-//                 var self = this;
-//                 var $container = $('<div class="leaflet-control leaflet-bar"></div>');
-//
-//                 var $link = $('<a href="#" title="Create a ' + this.options.kind + ' Geofence"></a>');
-//
-//                 $link.html(this.options.html);
-//
-//                 console.log(this, this.options);
-//
-//                 $link.on('click', this.options, function (e) {
-//                     e.preventDefault();
-//                     e.stopPropagation();
-//
-//                 //    var layer = e.data.callback.call(map.editTools);
-//
-//                     if (self.options.kind === 'marker') {
-//                         return add_marker_from_control(map.getCenter());
-//                     }
-//
-//                     if (self.options.kind === 'circle') {
-//                         var k = map.editTools.startCircle();
-//
-//                         k.setStyle({color: 'DarkRed'});
-//                         //return add_marker_from_control(map.getCenter());
-//
-//
-//                         // const polyShape = new GeofenceCircle({},{});
-//                         // self.options.callback(map.editTools);
-//                       /*  var shape = map.editTools.startCircle();
-//                         console.log('AAAA', shape);
-//                          shape.setStyle({
-//                                 opacity: 1,
-//                                 fillOpacity: 0.5,
-//                                 color: '#034001',
-//                                 fillColor: '#034001',
-//                                 disabledColor: '#a8a8a8',
-//                                 disabledFillColor: '#a8a8a8',
-//                                 shapeHoverBackground: '#1491CC',
-//                                 editingColor: '#FF9808',
-//                                 editingFillColor: '#FF9808'
-//                         });*/
-//                         // return map.editTools.startCircle(map.editTools)
-//                     }
-//
-//                     if (self.options.kind === 'polygon') {
-//                         console.log('draw_polygon');
-//                         //const polyShape = new GeofencePolygon({},{});
-//
-//                     }
-//
-//                     console.log('asd', this);
-//                     return false;
-//                 });
-//
-//                 $container.html($link);
-//
-//                 return $container[0];
-//             }
-//         });
-//         return leaflet;
-//     };
-//
-// // L.EditControl, CircleControl
-    module.mapHandler.addNewControlToExtended = function (parentControl, name, options) {
-        parentControl[name] = parentControl.extend({
-            options: options
-        });
+    module.mapHandler.addEditControlToLeaflet = function () {
 
-        return parentControl;
-    };
-    console.log('is ok');
-
-    return module;
-} (WB || {}));
-
-WB.globals = WB.globals || {};
-
-// TODO move away from GLOBAL scope
-// Variables
-
-function show_map(context) {
-
-    if (!context) {
-        return;
-    }
-
-
-    $('#navigationbar').css('height', window.innerHeight * 0.1);
-    $('#map').css('height', window.innerHeight * 0.9);
-
-    const defaultMapConf = {
-        editable: true,
-        zoomControl: false
-    };
-    if ('bounds' in context) {
-        if (WB.globals.map) {
-            WB.globals.map.fitBounds(context['bounds']);
-        } else {
-            WB.globals.map = L.map('map', defaultMapConf).fitBounds(context['bounds']);
-
-        }
-    } else if (('lat' in context) && ('lng' in context)) {
-        if (WB.globals.map) {
-            WB.globals.map.setView([context['lat'], context['lng']], 11);
-        } else {
-            WB.globals.map = L.map('map', defaultMapConf).setView([context['lat'], context['lng']], 11);
-
-        }
-    }
-    else {
-        if (WB.globals.map) {
-            WB.globals.map.setView([33.3, 44.3], 6);
-        } else {
-            WB.globals.map = L.map('map', defaultMapConf).setView([33.3, 44.3], 6);
-
-        }
-    }
-
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(WB.globals.map);
-
-    init_map(WB.globals.map);
-}
-
-
-function init_map(map) {
-
-   // const leaflet = WB.mapHandler.addEditControlToLeaflet();
-
-     new L.Control.Zoom({position: 'topright'}).addTo(map);
-
-L.EditControl = L.Control.extend({
+         leaflet.EditControl = L.Control.extend({
             options: {
                 position: 'topright',
                 callback: null,
@@ -241,7 +89,80 @@ L.EditControl = L.Control.extend({
                 return $container[0];
             }
         });
+        return leaflet;
+    };
+//
+// // L.EditControl, CircleControl
+    module.mapHandler.addNewControlToExtended = function (parentControl, name, options) {
+        parentControl[name] = parentControl.extend({
+            options: options
+        });
 
+        return parentControl;
+    };
+    console.log('is ok');
+
+    return module;
+} (WB || {}, L));
+
+WB.globals = WB.globals || {};
+
+// TODO move away from GLOBAL scope
+// Variables
+
+function show_map(context) {
+
+    if (!context) {
+        return;
+    }
+
+
+    $('#navigationbar').css('height', window.innerHeight * 0.1);
+    $('#map').css('height', window.innerHeight * 0.9);
+
+    const defaultMapConf = {
+        editable: true,
+        zoomControl: false
+    };
+    if ('bounds' in context) {
+        if (WB.globals.map) {
+            WB.globals.map.fitBounds(context['bounds']);
+        } else {
+            WB.globals.map = L.map('map', defaultMapConf).fitBounds(context['bounds']);
+
+        }
+    } else if (('lat' in context) && ('lng' in context)) {
+        if (WB.globals.map) {
+            WB.globals.map.setView([context['lat'], context['lng']], 11);
+        } else {
+            WB.globals.map = L.map('map', defaultMapConf).setView([context['lat'], context['lng']], 11);
+
+        }
+    }
+    else {
+        if (WB.globals.map) {
+            WB.globals.map.setView([33.3, 44.3], 6);
+        } else {
+            WB.globals.map = L.map('map', defaultMapConf).setView([33.3, 44.3], 6);
+
+        }
+    }
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(WB.globals.map);
+
+    init_map(WB.globals.map);
+}
+
+
+function init_map(map) {
+
+    const leaflet = WB.mapHandler.addEditControlToLeaflet();
+
+     new L.Control.Zoom({position: 'topright'}).addTo(map);
+
+/*
     L.NewCircleControl = L.EditControl.extend({
 
         options: {
@@ -251,7 +172,7 @@ L.EditControl = L.Control.extend({
             html: '⬤'
         }
 
-    });
+    });*/
 /*
     WB.mapHandler.addNewControlToExtended(L.EditControl, 'MarkerControl', {
             position: 'topright',
@@ -259,14 +180,14 @@ L.EditControl = L.Control.extend({
             kind: 'marker',
             html: '<p>m</p>'
         });*/
-/*
-    WB.mapHandler.addNewControlToExtended(L.EditControl, 'CircleControl', {
+
+    WB.mapHandler.addNewControlToExtended(leaflet.EditControl, 'CircleControl', {
             position: 'topright',
             callback: map.editTools.startCircle,
             kind: 'circle',
             html: '⬤'
         });
-
+/*
     WB.mapHandler.addNewControlToExtended(L.EditControl, 'PolygonControl', {
         position: 'topright',
         callback: map.editTools.startPolygon,
