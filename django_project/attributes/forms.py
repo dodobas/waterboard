2e107eb8d16c6541dc2e8a7bc38ba607d096f1ec
Dražen Odobašić <dodobas@quaternion.co>
2017-12-28 00:16:22 +0100

    Fix SelectOption rendering on AttributeForm

--- django_project/attributes/forms.py
+++ django_project/attributes/forms.py
@@ -6,54 +6,40 @@ from django import forms
 from .models import AttributeGroup, Attribute, AttributeOption
 
 
-class SelectWithTitles(forms.Select):
-    def __init__(self, queryset, *args, **kwargs):
-        super(SelectWithTitles, self).__init__(*args, **kwargs)
-        self.queryset = queryset
-
-    def render_option(self, selected_choices, option_value, option_label):
-        title = ''
-        if option_value != '':
-            try:
-                title = self.queryset.get(id=int(option_value)).description
-            except AttributeOption.DoesNotExist:
-                pass
-
-        return '<option value="{}" title="{}">{}</option>'.format(option_value, title, option_label)
-
-
 class GroupForm(forms.Form):
     def __init__(self, *args, **kwargs):
         attribute_group = kwargs.pop('attribute_group')
         super(GroupForm, self).__init__(*args, **kwargs)
 
-        criterias = Attribute.objects.filter(attribute_group=attribute_group)
+        attributes = Attribute.objects.filter(attribute_group=attribute_group)
 
-        for criteria in criterias:
-            if criteria.result_type == 'Integer':
-                self.fields[criteria.name] = forms.IntegerField()
+        for attr in attributes:
+            if attr.result_type == 'Integer':
+                self.fields[attr.name] = forms.IntegerField()
 
-            elif criteria.result_type == 'Decimal':
-                self.fields[criteria.name] = forms.DecimalField(decimal_places=2, max_digits=9)
+            elif attr.result_type == 'Decimal':
+                self.fields[attr.name] = forms.DecimalField(decimal_places=2, max_digits=9)
 
-            elif criteria.result_type == 'DropDown':
-                queryset = AttributeOption.objects.filter(attribute_id=criteria.id).order_by('position')
+            elif attr.result_type == 'DropDown':
+                attributeoptions = AttributeOption.objects.filter(attribute_id=attr.id).order_by('position')
                 criteria_information = []
-                for query in queryset:
+                for attropt in attributeoptions:
                     criteria_information.append(
-                        '<b>%s</b></br>%s<br>' % (query.option, query.description.replace('"', '\''))
+                        '<b>%s</b></br>%s<br>' % (attropt.option, attropt.description.replace('"', '\''))
                     )
 
-                self.fields[criteria.name] = forms.ModelChoiceField(
-                    queryset=queryset, widget=SelectWithTitles(queryset),
+                self.fields[attr.name] = forms.ChoiceField(
+                    choices=[(attropt.value, attropt.option) for attropt in attributeoptions],
                     label='<b>%s</b> <span class="question-mark" help="%s">?<span>' % (
-                        criteria.name, '<br>'.join(criteria_information)
+                        attr.name, '<br>'.join(criteria_information)
                     )
                 )
 
-            elif criteria.result_type == 'MultipleChoice':
-                self.fields[criteria.name] = forms.ModelMultipleChoiceField(
-                    queryset=AttributeOption.objects.filter(attribute_id=criteria.id).order_by('position')
+            elif attr.result_type == 'MultipleChoice':
+                attributeoptions = AttributeOption.objects.filter(attribute_id=attr.id).order_by('position')
+                
+                self.fields[attr.name] = forms.MultipleChoiceField(
+                    choices=[(attropt.value, attropt.option) for attropt in attributeoptions],
                 )
 
 
@@ -64,8 +50,7 @@ class Group(object):
 
 
 class AttributeForm(forms.Form):
-    # healthsite_id = forms.CharField(max_length=100)
-    # assessment_id = forms.CharField(max_length=100)
+    # feature_uuid = forms.CharField(max_length=100)
     name = forms.CharField(max_length=100, min_length=3)
     latitude = forms.CharField()
     longitude = forms.CharField()
