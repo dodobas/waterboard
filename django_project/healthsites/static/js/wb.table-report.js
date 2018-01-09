874f926c199360e404ebae5713d75ddc949f89b4
Kresimir Knezevic <kresimir.knezevic@protonmail.com>
2018-01-09 06:53:24 +0100

    Dirty fix table report table

--- django_project/healthsites/static/js/wb.table-report.js
+++ django_project/healthsites/static/js/wb.table-report.js
@@ -65,91 +65,6 @@ TableReport.prototype = {
     getSelectedRow: function (rowDomObj) {
         return this.reportTable.row(rowDomObj).data();
     },
-    getFormAsDomObject: function (data) {
-        var self = this;
-
-        var form = $(`<div class="bs-component">
-            <div class="panel panel-primary">
-                <div id="messages_wrapper"></div>
-                <div class="panel-heading panel-heading-without-padding">
-                    <h4>
-                        <i class="mdi-content-add-box"></i>
-                        Add Water Feature Assessment
-                    </h4>
-                </div>
-                <div class="panel-body" >
-                ${data}
-                </div>
-            </div>
-        </div>
-        `);
-
-        var rowData = self.getSelectedRow();
-
-        form.find('#update_button').on('click', function (e) {
-            e.preventDefault();
-            axUpdateAsessement(
-                rowData._feature_uuid,
-                self.parseForm(form),
-                (data) => {
-                    console.log('CB func', data);
-                },
-                (request, error) => {
-                    console.log('Err CB func', request.responseText);
-//                     showModalForm
-                    self.showModalForm(request.responseText)
-
-                    // form.html(request.responseText);
-                },
-            );
-
-        });
-        return form;
-    },
-    parseForm: function (content) {
-
-        var allGroups = content[0].querySelectorAll('[data-group-name]');
-
-        var values = {};
-
-        var groupsCnt = allGroups.length;
-
-        var groupName;
-
-        for (var i = 0; i < groupsCnt; i += 1) {
-
-            groupName = allGroups[i].dataset.groupName;
-
-            var inputs = allGroups[i].querySelectorAll('input, select');
-
-            for (var j = 0; j < inputs.length; j += 1) {
-                values[groupName + '/' + inputs[j].name] = inputs[j].value;
-            }
-
-        }
-
-        // parse hidden inputs
-        var hidden_inputs = document.getElementById('feature_hidden_data').querySelectorAll('input');
-
-        for (var h = 0; h < hidden_inputs.length; h += 1) {
-            values[hidden_inputs[h].name + ''] = hidden_inputs[h].value;
-        }
-
-        return values;
-    },
-    showModalForm: function (data) {
-        var content = this.getFormAsDomObject(data);
-        this.initAccordion(content);
-
-        WB.modal._setContent(content);
-        WB.modal._show();
-    },
-    initAccordion: function (parentDom) {
-        $(parentDom).find('#data-accordion').accordion({
-            heightStyle: "content",
-            header: this.accordionSelector
-        });
-    },
     addTableEvents: function () {
         var self = this;
 
@@ -162,15 +77,6 @@ TableReport.prototype = {
 
 
             openInNewTab('/feature-by-uuid/' + uuid);
-            // $.ajax({
-            //     url: url_update_feature(rowData.id),
-            //     success: function (data) {
-            //         self.showModalForm(data)
-            //     },
-            //     error: function (request, error) {
-            //         console.log(error);
-            //     }
-            // });
 
         });
 
