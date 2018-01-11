9f4cf55287e6515a0a548d1c807b9e56304e7f14
Kresimir Knezevic <kresimir.knezevic@protonmail.com>
2018-01-11 05:28:54 +0100

    add get_dashboard_fencing_count alt sample

--- django_project/sql_scripts/dashboard_query_alternatives_tmp.sql
+++ django_project/sql_scripts/dashboard_query_alternatives_tmp.sql
@@ -26,3 +26,32 @@ select
 
 ) row;
 
+
+-- *
+-- * tabiya,, fencing_exists, count
+-- * current function name: core_utils.get_dashboard_fencing_count
+-- *
+
+select
+    jsonb_agg(row)::text
+(
+
+    select
+        tabiya as group,
+        fencing_exists as fencing,
+        count(fencing_exists) cnt
+    FROM (
+        select
+            feature_uuid feature_uuid,
+            attribute_id as fencing_exists,
+            val_int as tabiya
+        from
+            features.feature_attribute_value fav
+        where
+            fav.attribute_id in (4, 23) and fav.is_active = True
+        order by 1,2
+    ) as fav
+    GROUP BY
+        fav.tabiya,
+        fav.fencing_exists
+) row;
