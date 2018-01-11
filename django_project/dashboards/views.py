7e8071ee9a952019b7a5c7bbc9e30f6f1758fab2
Kresimir Knezevic <kresimir.knezevic@protonmail.com>
2018-01-11 06:46:29 +0100

    Export dashboard data to dashboard html vars

--- django_project/dashboards/views.py
+++ django_project/dashboards/views.py
@@ -26,5 +26,29 @@ class DashboardView(TemplateView):
             )
             context['group_cnt'] = cur.fetchone()[0]
 
+            cur.execute(
+                'SELECT * FROM core_utils.get_dashboard_fencing_count(%s, %s, %s, %s)',
+                (-180, -90, 180, 90)
+            )
+            context['fencing_cnt'] = cur.fetchone()[0]
+
+            cur.execute(
+                'SELECT * FROM core_utils.get_dashboard_schemetype_count(%s, %s, %s, %s)',
+                (-180, -90, 180, 90)
+            )
+            context['schemetype_cnt'] = cur.fetchone()[0]
+
+            cur.execute(
+                'SELECT * FROM core_utils.get_dashboard_functioning_count(%s, %s, %s, %s)',
+                (-180, -90, 180, 90)
+            )
+            context['functioning_cnt'] = cur.fetchone()[0]
+
+            cur.execute(
+                'SELECT * FROM core_utils.get_dashboard_yieldgroup_count(%s, %s, %s, %s)',
+                (-180, -90, 180, 90)
+            )
+            context['yield_cnt'] = cur.fetchone()[0]
+
         return context
 
