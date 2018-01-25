5843684747e328ccd178ead45cc587ba98157ff9
Kresimir Knezevic <kresimir.knezevic@protonmail.com>
2018-01-25 23:25:52 +0100

    Horizontal Bar Chart Code clean

--- django_project/core/base_static/js/wb.chart.horizontalbar.js
+++ django_project/core/base_static/js/wb.chart.horizontalbar.js
@@ -1,82 +1,82 @@
+
 function barChartHorizontal(options) {
 
-    var data = options.data;
+    const {
+        data,
+        columns,
+        parentId = 'chart',
+        svgClass,
+        thickNmbr = 5,
+        xAxisClass = 'x axis',
+        yAxisClass = 'y axis',
+        barsClass,
+        toolTipClass = 'toolTip',
+        margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 80
+        }
 
-    var columns = options.columns;
-    var parentId = options.parentId || 'chart';
-    var svgClass = options.svgClass;
-
+    } = options;
 
     var parent = document.getElementById(parentId);
 
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
-    var w = (parent.getBoundingClientRect()).width;
-    var width = w || 960;
-    var height = options.height || 460;
 
-    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+    var svgWidth = options.width || (parent.getBoundingClientRect()).width || 960;
+    var svgHeight = options.height || 460;
 
-    var tooltip = d3.select('body').append("div").attr("class", "toolTip");
-
+    var tooltip = d3.select('body').append("div").attr("class", toolTipClass);
 
     var svg = d3.select('#' + parentId)
         .append('svg')
         .attr('class', svgClass)
-        .attr('width', width)
-        .attr('height', height);
+        .attr('width', svgWidth)
+        .attr('height', svgHeight);
 
-        var margin = {top: 20, right: 20, bottom: 30, left: 80};
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    width = width - margin.left - margin.right;
-    height = height - margin.top - margin.bottom;
-
-    // var g = svg.append('g')
-    //     .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
 
     var x = d3.scaleLinear().range([0, width]);
     var y = d3.scaleBand().range([height, 0]);
 
-    x.domain([0, d3.max(data, function(d) { return d.cnt; })]);
-    // columns
-    if (columns) {
-        y.domain(columns).padding(0.1);
-    } else {
-       y.domain(data.map(function(d) { return d.group; })).padding(0.1);
-    }
+    x.domain([0, d3.max(data, d => d.cnt )]);
 
+    columns ? y.domain(columns).padding(0.1) : y.domain(data.map( d => d.group )).padding(0.1);
 
     g.append("g")
-        .attr("class", "x axis")
-       	.attr("transform", "translate(0," + height + ")")
-      	.call(d3.axisBottom(x).ticks(5).tickFormat(function(d) { return parseInt(d); }).tickSizeInner([-height]));
+        .attr("class", xAxisClass)
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(thickNmbr).tickFormat(
+            (d) => parseInt(d)).tickSizeInner([-height])
+        );
 
     g.append("g")
-        .attr("class", "y axis")
+        .attr("class", yAxisClass)
         .call(d3.axisLeft(y));
 
-
-
-     g.selectAll(".bar")
+    g.selectAll(`.${barsClass}`)
         .data(data)
-      .enter().append("rect")
-        .attr("class", "bar")
+        .enter().append("rect")
+        .attr("class", barsClass)
         .attr("x", 0)
         .attr("height", y.bandwidth())
-        .attr("y", function(d) { return y(d.group); })
-        .attr("width", function(d) { return x(d.cnt); })
-        .on("mousemove", function(d){
+        .attr("y", d => y(d.group))
+        .attr("width", d => x(d.cnt))
+        .on("mousemove", (d) => {
             tooltip
-              .style("left", d3.event.pageX - 50 + "px")
-              .style("top", d3.event.pageY - 70 + "px")
-              .style("display", "inline-block")
-              .html((d.cnt) + "<br>" +  (d.group));
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY - 70 + "px")
+                .style("display", "inline-block")
+                .html('Count' + (d.cnt) + "<br>" + (d.group));
         })
-    		.on("mouseout", function(d){ tooltip.style("display", "none");});
-
+        .on("mouseout", (d) => tooltip.style("display", "none"));
 
     return {
         chart: svg
