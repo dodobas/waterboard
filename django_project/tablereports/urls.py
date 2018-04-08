55011f3b1c2152786b7500479d5e171951747ba3
Dražen Odobašić <dodobas@quaternion.co>
2018-04-08 12:55:20 +0200

    Add SHP File export for admin users

--- django_project/tablereports/urls.py
+++ django_project/tablereports/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from django.conf.urls import url
 
-from .views import CSVDownload, TableDataView, TableReportView
+from .views import CSVDownload, SHPDownload, TableDataView, TableReportView
 
 urlpatterns = (
     url(
@@ -14,5 +14,8 @@ urlpatterns = (
     ),
     url(
         r'^download-csv$', CSVDownload.as_view(), name='table.reports.csv_download'
+    ),
+    url(
+        r'^download-shp$', SHPDownload.as_view(), name='table.reports.shp_download'
     ),
 )
--- django_project/tablereports/views.py
+++ django_project/tablereports/views.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
+import shutil
+import tempfile
+import time
+from io import StringIO
+
+import fiona
+
+from django.contrib.gis.geos import WKBReader
 from django.db import connection
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
 
 from common.mixins import LoginRequiredMixin
-from common.utils import grouper
+from common.utils import UnicodeReader, grouper
 
 
 class TableReportView(LoginRequiredMixin, TemplateView):
@@ -77,9 +86,73 @@ class CSVDownload(LoginRequiredMixin, View):
 
             query = cur.fetchone()[0]
 
+            filename = 'waterpoints_{}.csv'. format(time.strftime('%Y%m%d_%H%M%S', time.gmtime()))
+
             response = HttpResponse(content_type='text/csv')
-            response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+            response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
 
             cur.copy_expert(query, response)
 
             return response
+
+
+class SHPDownload(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+
+        tempdir = tempfile.mkdtemp()
+
+        export_time = time.strftime('%Y%m%d_%H%M%S', time.gmtime())
+
+        with connection.cursor() as cur:
+            cur.execute("""
+                select * from  core_utils.export_all()
+            """)
+
+            query = cur.fetchone()[0]
+
+            data_buffer = StringIO()
+            cur.copy_expert(query, data_buffer)
+
+        # rewind the io object
+        data_buffer.seek(0)
+
+        point_data = UnicodeReader(data_buffer)
+
+        header = next(point_data)
+        # skip the first field, point_geom, trim to 10chars (SHP file limitation)
+        properties = [prop.upper()[:10] for prop in header[1:]]
+
+        # define basic geometry file properties
+        ogr_driver = 'ESRI Shapefile'
+        crs = {'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'}
+        schema = {
+            'geometry': 'Point',
+            'properties': {
+                prop: 'str' for prop in properties
+            }
+        }
+
+        shp_filename = os.path.join(tempdir, 'waterpoints_{}.shp'.format(export_time))
+
+        wkb_r = WKBReader()
+
+        with fiona.open(shp_filename, 'w', driver=ogr_driver, crs=crs, schema=schema) as new_shp:
+            for fields in point_data:
+                rec = dict()
+
+                rec['geometry'] = {u'type': u'Point', u'coordinates': wkb_r.read(fields[0]).coords}
+
+                rec['properties'] = {
+                    properties[idx]: value for idx, value in enumerate(fields[1:], start=0)
+                }
+
+                new_shp.write(record=rec)
+
+        # zip the directory
+        zip_filename = shutil.make_archive(tempfile.mktemp(), 'zip', tempdir)
+
+        response = HttpResponse(open(zip_filename, 'rb'), content_type='application/zip')
+        response['Content-Disposition'] = 'attachment; filename="waterpoints_{}.zip"'.format(export_time)
+
+        return response
