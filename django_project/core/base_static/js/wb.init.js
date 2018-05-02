595276832419c77c27e3b376185c102587b59020
Krešimir Knežević <kresimir.knezevic@protonmail.com>
2018-05-02 23:51:08 +0200

    Move create feature init to wb.init.js

--- django_project/core/base_static/js/wb.init.js
+++ django_project/core/base_static/js/wb.init.js
@@ -41,53 +41,131 @@ var WB = (function (module) {
     };
 
 
-
-     // Main Table reports page
+     // Main Table reports init function
     module.init.initTableReport = function (reportTableDataAttributes) {
+
         var dynamicColumns = reportTableDataAttributes.map(function (attribute) {
             return {
-              data: attribute.key,
-              title: '<div>' + attribute.label + '</div>',
-              searchable: attribute.searchable,
-              orderable: attribute.orderable
+                data: attribute.key,
+                title: '<div>' + attribute.label + '</div>',
+                searchable: attribute.searchable,
+                orderable: attribute.orderable
             };
-          });
+        });
 
-      var TABLE_REPORT_COLUMNS = [{
-        data: '_last_update',
-        title: 'Last Update',
-        searchable: false,
-        render: timestampColumnRenderer,
-        orderable: true
-      }, {
-        data: '_webuser',
-        title: 'User',
-        searchable: false,
-        orderable: true
-      }].concat(dynamicColumns);
+        var TABLE_REPORT_COLUMNS = [{
+            data: '_last_update',
+            title: 'Last Update',
+            searchable: false,
+            render: timestampColumnRenderer,
+            orderable: true
+        }, {
+            data: '_webuser',
+            title: 'User',
+            searchable: false,
+            orderable: true
+        }].concat(dynamicColumns);
 
-      var options = {
-        dataTable: {
-          "dom": 'l<"wb-export-toolbar">frtip',
-          scrollX: true,
-          fixedHeader: true,
-          columns: TABLE_REPORT_COLUMNS,
-          order: [[0, 'desc']],
-          lengthMenu: TABLE_ROWS_PER_PAGE,
-          rowClickCb: tableRowClickHandlerFn,
-          serverSide: true,
-          // this is only throttling and not debouncing, for debouncing we need to fully control search input events
-          searchDelay: 400,
-          ajax: {
-            url: '/table-data',
-            type: 'POST'
-          }
-        }
-      };
+        var options = {
+            dataTable: {
+                "dom": 'l<"wb-export-toolbar">frtip',
+                scrollX: true,
+                fixedHeader: true,
+                columns: TABLE_REPORT_COLUMNS,
+                order: [[0, 'desc']],
+                lengthMenu: TABLE_ROWS_PER_PAGE,
+                rowClickCb: tableRowClickHandlerFn,
+                serverSide: true,
+                // this is only throttling and not debouncing, for debouncing we need to fully control search input events
+                searchDelay: 400,
+                ajax: {
+                    url: '/table-data',
+                    type: 'POST'
+                }
+            }
+        };
 
-      module.tableReports.init('reports-table', options);
+        module.tableReports.init('reports-table', options);
     };
 
+     module.init.initUpdateFeature = function () {};
+
+
+     // Create Feature page init function
+     module.init.initCreateFeature = function () {
+         module.notif = module.SimpleNotification()
+          .options({
+            message: null,
+            type: 'success',
+            fadeOut: {
+              delay: Math.floor(Math.random() * 500) + 2500,
+              enabled: true
+            }
+          });
+        module.notif();
+
+        module.FeatureForm = new SimpleForm({
+          formId: 'feature-create-form',
+          parentId: 'formWrap',
+          isEnabled: true,
+          isBtnVisible: true,
+          submitBtnSelector: '#create_button',
+          onSubmit: function (formData, formInstance) {
+            module.loadingModal.show();
+            formInstance.formDomObj.submit();
+          },
+          accordionConf: {
+            selector: '#data-accordion',
+            opts: {
+              heightStyle: "content",
+              header: "div > h3"
+            }
+          }
+        });
+
+        // Leaflet Map
+
+        var coords = module.FeatureForm.getFormFieldValues(['_latitude', '_longitude']);
+
+        var markerGeometry = {lon: 38.3, lat: 14.3};
+
+        if (coords._longitude && coords._latitude) {
+          markerGeometry = {
+            lon: coords._longitude,
+            lat: coords._latitude
+          };
+        } else {
+          module.FeatureForm.setFormFieldValues({
+            _longitude: markerGeometry.lon,
+            _latitude: markerGeometry.lat
+          });
+        }
+
+
+        // setup
+        module.mapInstance = wbMap()
+          .layerConf(TILELAYER_DEFINITIONS)
+          .leafletConf({
+            zoom: 12,
+            editable: true
+          }, 'MapBox')
+          .markerRenderer(createFeatureByUUidMarker)
+          .initMapSearch({
+            parentId: 'geo-search-wrap'
+          })
+          .markerData([{
+            geometry: markerGeometry,
+            data: {},
+            draggable: true,
+            zoomToMarker: true
+          }]);
+
+        // init
+        module.mapInstance('featureMapWrap');
+
+        // add markers
+        module.mapInstance.renderMarkers({});
+     };
 
     return module;
 
