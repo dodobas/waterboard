2cda2bf9c6c3413cf37a593ef5940dd1f2620dba
Krešimir Knežević <kresimir.knezevic@protonmail.com>
2018-05-02 23:59:41 +0200

    Move feature by uuid init to wb.init.js

--- django_project/core/base_static/js/wb.init.js
+++ django_project/core/base_static/js/wb.init.js
@@ -88,7 +88,204 @@ var WB = (function (module) {
         module.tableReports.init('reports-table', options);
     };
 
-     module.init.initUpdateFeature = function () {};
+    // update init
+     module.init.initUpdateFeature = function (featureData, featureHistoryData, yieldData, staticWaterData) {
+
+module.notif = WB.SimpleNotification()
+        .options({
+          message: null,
+          type: 'success',
+          fadeOut: {
+            delay: Math.floor(Math.random() * 500) + 2500,
+            enabled: true
+          }
+        });
+
+      // init notification
+      module.notif();
+            // LINE CHARTS
+            var chart_yield = lineChart({
+              data: yieldData,
+              parentId: 'chartWrap-yield',
+              title: 'Yield',
+              svgClass: 'wb-line-chart',
+              height: 250,
+              yLabel: 'Yield',
+              labelField: 'ts',
+              valueField: 'value'
+            });
+
+            var chart_static = lineChart({
+              data: staticWaterData,
+              parentId: 'chartWrap-static',
+              svgClass: 'wb-line-chart',
+              height: 250,
+              yLabel: 'Water Level',
+              labelField: 'ts',
+              valueField: 'value'
+            });
+
+            // charts on resize
+            d3.select(window).on('resize', _.debounce(function () {
+              chart_yield.resize();
+              chart_static.resize();
+            }, 250));
+
+
+            // MAP
+
+            // setup map
+            module.mapInstance = wbMap({})
+              .layerConf(TILELAYER_DEFINITIONS)
+              .leafletConf({
+                zoom: 12,
+                editable: true
+              }, 'MapBox')
+              .markerRenderer(createFeatureByUUidMarker)
+              .initMapSearch({
+                parentId: 'geo-search-wrap'
+              })
+              .markerData([{
+                geometry: {
+                  lon: featureData._geometry[0],
+                  lat: featureData._geometry[1]
+                },
+                data: featureData,
+                draggable: false,
+                zoomToMarker: true
+              }]);
+
+            // init map
+            module.mapInstance('featureMapWrap');
+
+            module.mapInstance.renderMarkers({});
+
+
+            // FEATURE FORM
+
+
+
+            module.FeatureForm = new SimpleForm({
+              formId: 'add_even_form',
+              parentId: 'formWrap',
+              submitBtnSelector: '#update_button',
+              isBtnVisible: false,
+              onSubmit: function (formData) {
+                module.api.axUpdateFeature({
+                  data: module.utils.removeBlacklistedPropsFromObject({
+                    flatObj: formData
+                  })
+                });
+              },
+              isEnabled: false,
+              accordionConf: {
+                selector: '#data-accordion',
+                opts: {
+                  heightStyle: "content",
+                  header: "div > h3"
+                }
+              }
+            });
+
+            // toggle-update-form
+
+            var formToggleBtn = document.getElementById('toggle-update-form');
+
+            module.utils.addEvent(formToggleBtn, 'click', function (e) {
+
+              var label, style;
+
+              var markers = module.mapInstance.markerLayer().getLayers();
+
+              var lastMarker = markers[markers.length - 1];
+
+
+              if (module.FeatureForm.enableForm()) {
+                style = true;
+                label = 'Enable edit';
+
+                lastMarker.dragging.enable();
+              } else {
+                style = false;
+                label = 'Disable edit';
+
+                lastMarker.dragging.disable();
+              }
+
+              module.FeatureForm.showUpdateButton(style);
+
+              // change button label
+              this.innerHTML = label;
+
+            });
+
+            // Init Modal Class
+            module.modal = new module.Modal({});
+
+            // History Table
+
+            var options = {
+              dataTable: {
+                data: featureHistoryData,
+                fixedHeader: true,
+                searching: false,
+                columns: [{
+                  data: 'ts',
+                  title: 'Last update',
+                  orderable: true,
+                  render: timestampColumnRenderer
+                }, {
+                  data: 'email',
+                  title: 'User',
+                  orderable: true
+                }, {
+                  data: 'static_water_level',
+                  title: 'SWL',
+                  orderable: true
+                }, {
+                  data: 'yield',
+                  title: 'YLD',
+                  orderable: true
+                }],
+                order: [[0, "desc"]],
+                lengthMenu: TABLE_ROWS_PER_PAGE_SMALL,
+                rowClickCb: function (row) {
+
+                  var key = (row.feature_uuid_id.split('_') || []);
+
+                  module.api.axGetFeatureChangesetByUUID({
+                    featureUUID: key[0],
+                    changesetId: key[1]
+                  });
+                },
+
+              },
+              modalOpts: {
+                title: 'History Data',
+                modalOnOpenCb: function (data) {
+
+                  initAccordion({
+                    selector: '#wb-dialog div#data-accordion',
+                    opts: {
+                      heightStyle: "content",
+                      header: "div > h3"
+                    }
+                  });
+                  var modalDomObj = data.modalObj;
+
+                  $(modalDomObj).find('fieldset').attr({disabled: true});
+
+                  $(modalDomObj).find('#update_button').hide();
+                }
+              }
+            };
+
+            // This instance has a modal attached to self (some vals are hardcoded)
+            // Todo update
+            module.historytable = module.tableReports.init('history-table', options);
+
+     };
+     // end update init
 
 
      // Create Feature page init function
