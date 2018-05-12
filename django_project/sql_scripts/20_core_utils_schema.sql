2cad1b16b5c0e781fc119f01a10972c785384a04
Krešimir Knežević <kresimir.knezevic@protonmail.com>
2018-05-12 14:25:32 +0200

    pg fncs separation, refactor... ongoing

--- django_project/sql_scripts/20_core_utils_schema.sql
+++ django_project/sql_scripts/20_core_utils_schema.sql
@@ -1,6 +1,6 @@
 
 -- *
--- core_utils.get_features
+-- core_utils.get_features, used od features / table reports
 -- *
 
 CREATE OR REPLACE FUNCTION core_utils.get_features(i_webuser_id integer, i_limit integer, i_offset integer, i_order_text text, i_search_name text)
@@ -71,7 +71,7 @@ $fun$;
 
 
 -- *
--- core_utils.create_changeset
+-- core_utils.create_changeset, used in attributes / features
 -- *
 
 CREATE OR REPLACE FUNCTION core_utils.create_changeset(i_webuser_id integer)
@@ -91,7 +91,7 @@ $$;
 
 
 -- *
--- core_utils.add_feature
+-- core_utils.add_feature, used in attributes/views
 -- *
 
 CREATE or replace FUNCTION core_utils.add_feature(i_feature_uuid uuid, i_feature_changeset integer, i_feature_point_geometry geometry, i_feature_attributes text)
@@ -232,7 +232,7 @@ and
     changeset_id != i_feature_changeset;
 
     -- update active data / TODO use a rule instead ?
-   -- execute core_utils.update_active_data_row(i_feature_uuid);
+   execute core_utils.update_active_data_row(i_feature_uuid);
 
     RETURN core_utils.get_event(i_feature_uuid);
 END;
@@ -240,7 +240,7 @@ $$;
 
 
 -- *
--- core_utils.get_attributes
+-- core_utils.get_attributes, used in table reports
 -- *
 
 
@@ -261,7 +261,7 @@ $$;
 
 
 
-
+-- used in attribite / features
 CREATE OR REPLACE FUNCTION core_utils.get_event(i_uuid UUID, i_changeset_id int default null )
     RETURNS TEXT AS
 $BODY$
@@ -347,7 +347,7 @@ COST 100;
 
 
 -- *
--- core_utils.get_attribute_history_by_uuid
+-- core_utils.get_attribute_history_by_uuid, used in features
 -- *
 
 create or replace function
@@ -586,7 +586,7 @@ $$;
 -- *
 -- * DROP attributes attribute column active_data
 -- *
-CREATE OR REPLACE FUNCTION core_utils.drop_attribute(old ATTRIBUTES_ATTRIBUTE)
+CREATE OR REPLACE FUNCTION core_utils.drop_attribute(i_old ATTRIBUTES_ATTRIBUTE)
     RETURNS VOID
 LANGUAGE plpgsql
 AS $$
@@ -595,7 +595,7 @@ DECLARE
     l_field_name TEXT;
 BEGIN
 
-    SELECT old.key AS field_name
+    SELECT i_old.key AS field_name
     INTO
         l_field_name;
 
@@ -613,7 +613,7 @@ $$;
 -- *
 -- * Add attributes attribute column active_data
 -- *
-create or replace function core_utils.add_attribute(new attributes_attribute)
+create or replace function core_utils.add_attribute(i_new attributes_attribute)
    RETURNS void
 LANGUAGE plpgsql
 AS $$
@@ -625,13 +625,13 @@ BEGIN
 
   select
     case
-          when new.result_type = 'Integer' THEN 'int'
-          when new.result_type = 'Decimal' THEN 'float'
-          when new.result_type = 'Text' THEN 'text'
-          when new.result_type = 'DropDown' THEN 'text'
+          when i_new.result_type = 'Integer' THEN 'int'
+          when i_new.result_type = 'Decimal' THEN 'float'
+          when i_new.result_type = 'Text' THEN 'text'
+          when i_new.result_type = 'DropDown' THEN 'text'
           ELSE null
          end as val,
-         new.key as field_name
+         i_new.key as field_name
   into
     l_attribute_type, l_field_name;
 
@@ -644,154 +644,3 @@ BEGIN
 
 end
 $$;
-
-
--- 'Unique ID', 'unique_id', 1, 'Text', 0, TRUE, TRUE, FALSE
-
-CREATE OR REPLACE FUNCTION core_utils.load_text_attribute(
-    i_field_id text, i_label text, i_key text, i_attr_group_id integer, i_required boolean, i_orderable boolean, i_searchable boolean, i_position integer
-) RETURNS void
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    l_attr_id integer;
-BEGIN
-
-    INSERT INTO
-    public.attributes_attribute (label, key, attribute_group_id, result_type, position, required, orderable, searchable)
-VALUES (i_label, i_key, i_attr_group_id, 'Text', i_position, i_required, i_orderable, i_searchable) RETURNING id INTO l_attr_id;
-
-
-execute format($r$INSERT INTO features.feature_attribute_value(
-    feature_uuid,
-    attribute_id,
-    val_text,
-    changeset_id
-) SELECT
-    ft.feature_uuid as feature_uuid,
-    %L as attribute_id,
-    ird.%s,
-    1 as changeset_id
-FROM
-    test_data.import_raw_data_2 ird
-    JOIN
-    features.feature ft ON ird.id=ft.upstream_id;$r$, l_attr_id, i_field_id);
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION core_utils.load_integer_attribute(
-    i_field_id text, i_label text, i_key text, i_attr_group_id integer, i_required boolean, i_orderable boolean, i_searchable boolean, i_position integer
-) RETURNS void
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    l_attr_id integer;
-BEGIN
-
-    INSERT INTO
-    public.attributes_attribute (label, key, attribute_group_id, result_type, position, required, orderable, searchable)
-VALUES (i_label, i_key, i_attr_group_id, 'Integer', i_position, i_required, i_orderable, i_searchable) RETURNING id INTO l_attr_id;
-
-
-execute format($r$INSERT INTO features.feature_attribute_value(
-    feature_uuid,
-    attribute_id,
-    val_int,
-    changeset_id
-) SELECT
-    ft.feature_uuid as feature_uuid,
-    %L as attribute_id,
-    cast(ird.%s as int),
-    1 as changeset_id
-FROM
-    test_data.import_raw_data_2 ird
-    JOIN
-    features.feature ft ON ird.id=ft.upstream_id$r$, l_attr_id, i_field_id);
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION core_utils.load_decimal_attribute(
-    i_field_id text, i_label text, i_key text, i_attr_group_id integer, i_required boolean, i_orderable boolean, i_searchable boolean, i_position integer
-) RETURNS void
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    l_attr_id integer;
-BEGIN
-
-    INSERT INTO
-    public.attributes_attribute (label, key, attribute_group_id, result_type, position, required, orderable, searchable)
-VALUES (i_label, i_key, i_attr_group_id, 'Decimal', i_position, i_required, i_orderable, i_searchable) RETURNING id INTO l_attr_id;
-
-
-execute format($r$INSERT INTO features.feature_attribute_value(
-    feature_uuid,
-    attribute_id,
-    val_real,
-    changeset_id
-) SELECT
-    ft.feature_uuid as feature_uuid,
-    %L as attribute_id,
-    cast(ird.%s as double precision),
-    1 as changeset_id
-FROM
-    test_data.import_raw_data_2 ird
-    JOIN
-    features.feature ft ON ird.id=ft.upstream_id;$r$, l_attr_id, i_field_id);
-END;
-$$;
-
-
-CREATE OR REPLACE FUNCTION core_utils.load_dropdown_attribute(
-    i_field_id text, i_label text, i_key text, i_attr_group_id integer, i_required boolean, i_orderable boolean, i_searchable boolean, i_position integer
-) RETURNS void
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    l_attr_id integer;
-BEGIN
-
-    INSERT INTO
-    public.attributes_attribute (label, key, attribute_group_id, result_type, position, required, orderable, searchable)
-        VALUES (i_label, i_key, i_attr_group_id, 'DropDown', i_position, i_required, i_orderable, i_searchable) RETURNING id INTO l_attr_id;
-
-    execute format($r$INSERT INTO
-        public.attributes_attributeoption (option, value, description, position, attribute_id)
-    select
-        r.option
-        , row_number() OVER (ORDER BY r.option)
-        , r.description
-        , row_number() OVER (ORDER BY r.option)
-        , r.attribute_id
-    from (
-        select
-        coalesce(substr(initcap(%s), 1, 128), 'Unknown') as option,
-        '' as description,
-        %L::int as attribute_id
-        from test_data.import_raw_data_2
-        group by coalesce(substr(initcap(%s), 1, 128), 'Unknown')
-        ORDER BY 1) r;$r$, i_field_id, l_attr_id, i_field_id);
-
-    execute format($r$INSERT INTO features.feature_attribute_value(
-    feature_uuid,
-    attribute_id,
-    val_int,
-    changeset_id
-) SELECT
-    ft.feature_uuid as feature_uuid,
-    %L as attribute_id,
-    aao.value as val_int,
-    1 as changeset_id
-FROM
-    test_data.import_raw_data_2 ird JOIN features.feature ft
-        ON ird.id=ft.upstream_id
-    JOIN public.attributes_attributeoption aao
-        ON coalesce(substr(initcap(ird.%s), 1, 128), 'Unknown') = aao.option AND aao.attribute_id = %L;$r$, l_attr_id, i_field_id, l_attr_id);
-
-END;
-$$;
-
-
-
-
-
