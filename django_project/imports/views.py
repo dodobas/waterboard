d81d49d20520ba8c200a5ab84b9224546236c5e2
Frane Gilic <franegilic@outlook.com>
2018-08-24 22:54:49 +0200

    Enable downloading uploaded files in Import History page

--- django_project/imports/views.py
+++ django_project/imports/views.py
@@ -181,11 +181,12 @@ class ImportHistory(LoginRequiredMixin, View):
                 task_history_states = cursor.fetchall()
 
         history_list = []
-        for task_id, file_name, changed_at, new_state, _ in task_history_states:
+        for task_id, file_path, changed_at, new_state, _ in task_history_states:
             if new_state == TaskHistory.STATE_UPLOADED:
-                file_name = split(file_name)[1]
+                file_name = split(file_path)[1]
                 history_list.append(
-                    {'task_id': task_id, 'updated_at': changed_at, 'file_name': file_name, 'imported_at': None})
+                    {'task_id': task_id, 'updated_at': changed_at, 'file_name': file_name, 'imported_at': None,
+                     'file_path': file_path})
 
         for task_id, _, changed_at, new_state, _ in task_history_states:
             if new_state == TaskHistory.STATE_INSERTED:
