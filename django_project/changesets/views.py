02fc75149c945cfe330aa55efcc8a1e7112330e5
Frane Gilic <franegilic@outlook.com>
2018-08-27 11:18:32 +0200

    List all changesets in Changeset Explorer page

--- django_project/changesets/views.py
+++ django_project/changesets/views.py
@@ -2,28 +2,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from django.db import connection, transaction
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.views import View
 
-from common.mixins import LoginRequiredMixin
+from common.mixins import AdminRequiredMixin
 
 
-class ChangesetsExplorerView(LoginRequiredMixin, View):
+class ChangesetsExplorerView(AdminRequiredMixin, View):
 
     def get(self, request):
-        if not request.user.is_staff:
-            return redirect('admin-control/')
 
         with transaction.atomic():
             with connection.cursor() as cursor:
                 cursor.execute(
                     """
-                    SELECT changeset_id, to_char(changed_at, 'YYYY-MM-DD HH24:MI:SS TZ'), email
-                    FROM (public.imports_task INNER JOIN public.webusers_webuser
-                        ON webusers_webuser.id = imports_task.webuser_id) INNER JOIN public.imports_taskhistory
-                        ON imports_task.id = imports_taskhistory.task_id
-                    WHERE new_state = 'i'
-                    ORDER BY changed_at DESC;
+                    SELECT features.changeset.id, to_char(ts_created, 'YYYY-MM-DDT_HH24:MI:SSZ'), email
+                    FROM features.changeset INNER JOIN public.webusers_webuser
+                    ON features.changeset.webuser_id = public.webusers_webuser.id
+                    ORDER BY ts_created DESC;
                     """
                 )
 
@@ -37,12 +33,9 @@ class ChangesetsExplorerView(LoginRequiredMixin, View):
         return render(request, 'changesets/changeset_explorer_page.html', {'changesets_list': changesets_list})
 
 
-class ChangesetReportView(LoginRequiredMixin, View):
+class ChangesetReportView(AdminRequiredMixin, View):
 
     def get(self, request, changeset_id):
-        if not request.user.is_staff:
-            return redirect('/admin-control/')
-
         with connection.cursor() as cur:
             cur.execute('select * from core_utils.get_attributes()')
             attributes = cur.fetchone()[0]
