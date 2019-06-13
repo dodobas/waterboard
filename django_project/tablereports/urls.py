614b60d5296dcf8db4cf42bd9f82afdd83193407
Dražen Odobašić <dodobas@quaternion.co>
2019-06-13 11:02:20 +0200

    Clean tablereports, remove dead code

--- django_project/tablereports/urls.py
+++ django_project/tablereports/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import TableDataView, TableReportView
+from .views import TableReportView
 
 app_name = 'tablereports'
 
 urlpatterns = (
     path('table-report/', TableReportView.as_view(), name='table.reports.view'),
-    path('table-data/', TableDataView.as_view(), name='table.data.view')
 )
--- django_project/tablereports/views.py
+++ django_project/tablereports/views.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import connection
-from django.http import HttpResponse
-from django.views import View
 from django.views.generic import TemplateView
-
-from common.utils import grouper
 
 
 class TableReportView(LoginRequiredMixin, TemplateView):
@@ -21,54 +17,3 @@ class TableReportView(LoginRequiredMixin, TemplateView):
         context.update({'attributes': attributes})
 
         return context
-
-
-class TableDataView(LoginRequiredMixin, View):
-
-    def post(self, request, *args, **kwargs):
-
-        def parse_column_data(index):
-            key = 'columns[{}][data]'.format(index)
-            return request.POST.get(key)
-
-        # TODO: datatables uses draw count to distinguish between requests
-        # draw = int(request.POST.get('draw', -1))
-
-        limit = int(request.POST.get('length', 10))
-        offset = int(request.POST.get('start', 0))
-        changeset_id = request.POST.get('changeset_id')
-
-        # TODO: use pg_trgm extension to handle unbounded LIKE searches???, speed is not a problem with 18000 features
-        search_values = request.POST.get('search[value]', '').split(' ')
-
-        if search_values:
-            search_predicate = 'WHERE '
-
-            search_predicates = (
-                f"zone||' '||woreda||' '||tabiya||' '||kushet||' '||coalesce(name, '')||' '||coalesce(unique_id, '') ILIKE '%{search_value}%'"
-                for search_value in search_values
-            )
-
-            search_predicate += ' AND '.join(search_predicates)
-        else:
-            search_predicate = None
-
-        order_keys = sorted([key for key in request.POST.keys() if key.startswith('order[')])
-
-        order_text = ', '.join(
-            '{} {}'.format(parse_column_data(request.POST.get(col)), request.POST.get(dir))
-            for col, dir in grouper(order_keys, 2)
-            if request.POST.get(dir) in ('asc', 'desc')  # poor mans' security
-        )
-
-        if order_text:
-            order_text = 'ORDER BY {}'.format(order_text)
-
-        with connection.cursor() as cur:
-            cur.execute(
-                'select data from core_utils.get_features(%s, %s, %s, %s, %s, %s) as data;',
-                (self.request.user.id, limit, offset, order_text, search_predicate, changeset_id)
-            )
-            data = cur.fetchone()[0]
-
-        return HttpResponse(content=data, content_type='application/json')
